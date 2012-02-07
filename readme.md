7a6b9434c8d3af77d9a3f533a1564370ced1d6aa
Josua Krause <josua.krause@googlemail.com>
2012-02-07 13:20:31 +0100

    formatted readme

--- readme.md
+++ readme.md
@@ -1,4 +1,3 @@
-== NBT Editor ==
 
 The NBT Editor is an easy to use NBT editor.
 The NBT file format is used by minecraft to store
@@ -8,60 +7,60 @@ The content of a NBT file is a gzipped stream of
 byte data. The following grammar is a complete
 description of the format:
 
-  RECORD := END | REAL_RECORD
-  
-  REAL_RECORD := BYTE | SHORT | INT | LONG | FLOAT
-               | DOUBLE | ARRAY | STRING | LIST | COMPOUND
-
-  END := 0
-
-  BYTE := 1 NAME RAW_BYTE
-
-  SHORT := 2 NAME RAW_SHORT
-
-  INT := 3 NAME RAW_INT
-
-  LONG := 4 NAME RAW_LONG
-
-  FLOAT := 5 NAME RAW_FLOAT
-
-  DOUBLE := 6 NAME RAW_DOUBLE
-
-  ARRAY := 7 NAME RAW_ARRAY
-
-  STRING := 8 NAME RAW_STRING
-
-  LIST := 9 NAME RAW_LIST
-  
-  COMPOUND := 10 NAME RAW_COMPOUND
-
-  NAME := RAW_STRING
-
-  RAW_BYTE := <signed byte>
-  
-  RAW_SHORT := <big endian signed short>
-  
-  RAW_INT := <big endian signed integer>
-  
-  RAW_LONG := <big endian signed long>
-  
-  RAW_FLOAT := <big endian IEEE 754-2008 float>
-
-  RAW_DOUBLE := <big endian IEEE 754-2008 double>
-
-  RAW_ARRAY := RAW_INT <byte array with length given by integer>
-
-  RAW_STRING := RAW_SHORT <utf8 string with length given by short>
-
-  RAW_LIST := [0-10] RAW_INT ITEM
-  // type of item given by byte and length given by integer
-  ITEM := RAW_ITEM (ITEM |)
-  
-  RAW_ITEM := RAW_BYTE | RAW_SHORT | RAW_INT | RAW_LONG | RAW_FLOAT
-            | RAW_DOUBLE | RAW_ARRAY | RAW_STRING | RAW_LIST | RAW_COMPOUND
-  
-  // names within a compound must be unique
-  RAW_COMPOUND := REAL_RECORD RAW_COMPOUND | END
+    RECORD := END | REAL_RECORD
+    
+    REAL_RECORD := BYTE | SHORT | INT | LONG | FLOAT
+                 | DOUBLE | ARRAY | STRING | LIST | COMPOUND
+    
+    END := 0
+    
+    BYTE := 1 NAME RAW_BYTE
+    
+    SHORT := 2 NAME RAW_SHORT
+    
+    INT := 3 NAME RAW_INT
+    
+    LONG := 4 NAME RAW_LONG
+    
+    FLOAT := 5 NAME RAW_FLOAT
+    
+    DOUBLE := 6 NAME RAW_DOUBLE
+    
+    ARRAY := 7 NAME RAW_ARRAY
+    
+    STRING := 8 NAME RAW_STRING
+    
+    LIST := 9 NAME RAW_LIST
+    
+    COMPOUND := 10 NAME RAW_COMPOUND
+    
+    NAME := RAW_STRING
+    
+    RAW_BYTE := <signed byte>
+    
+    RAW_SHORT := <big endian signed short>
+    
+    RAW_INT := <big endian signed integer>
+    
+    RAW_LONG := <big endian signed long>
+    
+    RAW_FLOAT := <big endian IEEE 754-2008 float>
+    
+    RAW_DOUBLE := <big endian IEEE 754-2008 double>
+    
+    RAW_ARRAY := RAW_INT <byte array with length given by integer>
+    
+    RAW_STRING := RAW_SHORT <utf8 string with length given by short>
+    
+    RAW_LIST := [0-10] RAW_INT ITEM
+    // type of item given by byte and length given by integer
+    ITEM := RAW_ITEM (ITEM |)
+    
+    RAW_ITEM := RAW_BYTE | RAW_SHORT | RAW_INT | RAW_LONG | RAW_FLOAT
+              | RAW_DOUBLE | RAW_ARRAY | RAW_STRING | RAW_LIST | RAW_COMPOUND
+    
+    // names within a compound must be unique
+    RAW_COMPOUND := REAL_RECORD RAW_COMPOUND | END
 
 The start symbol is RECORD and all numeric symbols stand for
 the byte representation.
--- src/nbt/gui/NBTEdit.java
+++ src/nbt/gui/NBTEdit.java
@@ -27,135 +27,136 @@ import nbt.write.NBTWriter;
 
 public class NBTEdit extends JPanel {
 
-    private static final long serialVersionUID = 6117715159789114581L;
+	private static final long serialVersionUID = 6117715159789114581L;
 
-    private final JLabel name;
+	private final JLabel name;
 
-    private final JTextField text;
+	private final JTextField text;
 
-    private TreePath path;
+	private TreePath path;
 
-    private NBTRecord cur;
+	private NBTRecord cur;
 
-    private File file;
+	private File file;
 
-    public NBTEdit(final JTree tree, final NBTFrame frame) {
-        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-        name = new JLabel(" ");
-        text = new JTextField();
-        cur = null;
-        add(name);
-        add(text);
-        final Action action = new AbstractAction("Edit") {
+	public NBTEdit(final JTree tree, final NBTFrame frame) {
+		setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+		name = new JLabel(" ");
+		text = new JTextField();
+		cur = null;
+		add(name);
+		add(text);
+		final Action action = new AbstractAction("Edit") {
 
-            private static final long serialVersionUID = -5082168158857360395L;
+			private static final long serialVersionUID = -5082168158857360395L;
 
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                try {
-                    cur.parsePayload(text.getText());
-                } catch (final ParseException ex) {
-                }
-                final NBTRecord r = (NBTRecord) tree.getModel().getRoot();
-                tree.getModel().valueForPathChanged(new TreePath(r), r);
-                text.setText(cur.getParseablePayload());
-                frame.setTitle(file, r.hasChanged());
-            }
+			@Override
+			public void actionPerformed(final ActionEvent e) {
+				try {
+					cur.parsePayload(text.getText());
+				} catch (final ParseException ex) {
+					// record has not changed
+				}
+				final NBTRecord r = (NBTRecord) tree.getModel().getRoot();
+				tree.getModel().valueForPathChanged(new TreePath(r), r);
+				text.setText(cur.getParseablePayload());
+				frame.setTitle(file, r.hasChanged());
+			}
 
-        };
-        add(new JButton(action));
-        text.addKeyListener(new KeyAdapter() {
+		};
+		add(new JButton(action));
+		text.addKeyListener(new KeyAdapter() {
 
-            @Override
-            public void keyReleased(final KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    action.actionPerformed(null);
-                }
-            }
+			@Override
+			public void keyReleased(final KeyEvent e) {
+				if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+					action.actionPerformed(null);
+				}
+			}
 
-        });
-        add(new JButton(new AbstractAction("Open") {
+		});
+		add(new JButton(new AbstractAction("Open") {
 
-            private static final long serialVersionUID = 4663994095611490449L;
+			private static final long serialVersionUID = 4663994095611490449L;
 
-            @Override
-            public void actionPerformed(final ActionEvent ae) {
-                final JFileChooser fc = new JFileChooser();
-                final int returnVal = fc.showOpenDialog(NBTEdit.this
-                        .getParent());
-                if (returnVal != JFileChooser.APPROVE_OPTION) {
-                    return;
-                }
-                file = fc.getSelectedFile();
-                NBTReader read = null;
-                NBTRecord r = null;
-                try {
-                    read = new NBTReader(file);
-                    r = read.read();
-                } catch (final IOException e) {
-                    e.printStackTrace();
-                } finally {
-                    if (read != null) {
-                        try {
-                            read.close();
-                        } catch (final IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                }
-                if (r != null) {
-                    tree.setModel(new NBTModel(r));
-                    frame.setTitle(file, r.hasChanged());
-                }
-            }
+			@Override
+			public void actionPerformed(final ActionEvent ae) {
+				final JFileChooser fc = new JFileChooser();
+				final int returnVal = fc.showOpenDialog(NBTEdit.this
+						.getParent());
+				if (returnVal != JFileChooser.APPROVE_OPTION) {
+					return;
+				}
+				file = fc.getSelectedFile();
+				NBTReader read = null;
+				NBTRecord r = null;
+				try {
+					read = new NBTReader(file);
+					r = read.read();
+				} catch (final IOException e) {
+					e.printStackTrace();
+				} finally {
+					if (read != null) {
+						try {
+							read.close();
+						} catch (final IOException e) {
+							e.printStackTrace();
+						}
+					}
+				}
+				if (r != null) {
+					tree.setModel(new NBTModel(r));
+					frame.setTitle(file, r.hasChanged());
+				}
+			}
 
-        }));
-        add(new JButton(new AbstractAction("Save") {
+		}));
+		add(new JButton(new AbstractAction("Save") {
 
-            private static final long serialVersionUID = -7308700550861060140L;
+			private static final long serialVersionUID = -7308700550861060140L;
 
-            @Override
-            public void actionPerformed(final ActionEvent ae) {
-                if (file == null) {
-                    return;
-                }
-                final NBTRecord r = (NBTRecord) tree.getModel().getRoot();
-                if (!r.hasChanged()) {
-                    return;
-                }
-                try {
-                    final NBTWriter write = new NBTWriter(file);
-                    write.write(r);
-                    write.close();
-                } catch (final IOException e) {
-                    e.printStackTrace();
-                }
-                r.resetChange();
-                tree.getModel().valueForPathChanged(new TreePath(r), r);
-                frame.setTitle(file, r.hasChanged());
-            }
+			@Override
+			public void actionPerformed(final ActionEvent ae) {
+				if (file == null) {
+					return;
+				}
+				final NBTRecord r = (NBTRecord) tree.getModel().getRoot();
+				if (!r.hasChanged()) {
+					return;
+				}
+				try {
+					final NBTWriter write = new NBTWriter(file);
+					write.write(r);
+					write.close();
+				} catch (final IOException e) {
+					e.printStackTrace();
+				}
+				r.resetChange();
+				tree.getModel().valueForPathChanged(new TreePath(r), r);
+				frame.setTitle(file, r.hasChanged());
+			}
 
-        }));
-        tree.getSelectionModel().setSelectionMode(
-                TreeSelectionModel.SINGLE_TREE_SELECTION);
-        tree.addTreeSelectionListener(new TreeSelectionListener() {
+		}));
+		tree.getSelectionModel().setSelectionMode(
+				TreeSelectionModel.SINGLE_TREE_SELECTION);
+		tree.addTreeSelectionListener(new TreeSelectionListener() {
 
-            @Override
-            public void valueChanged(final TreeSelectionEvent e) {
-                path = e.getPath();
-                final NBTRecord r = (NBTRecord) path.getLastPathComponent();
-                final String n = r.getName();
-                name.setText(n != null ? n + ": " : " ");
-                if (r.isTextEditable()) {
-                    text.setEnabled(true);
-                    text.setText(r.getParseablePayload());
-                } else {
-                    text.setText("");
-                    text.setEnabled(false);
-                }
-                cur = r;
-            }
+			@Override
+			public void valueChanged(final TreeSelectionEvent e) {
+				path = e.getPath();
+				final NBTRecord r = (NBTRecord) path.getLastPathComponent();
+				final String n = r.getName();
+				name.setText(n != null ? n + ": " : " ");
+				if (r.isTextEditable()) {
+					text.setEnabled(true);
+					text.setText(r.getParseablePayload());
+				} else {
+					text.setText("");
+					text.setEnabled(false);
+				}
+				cur = r;
+			}
 
-        });
-    }
+		});
+	}
 }
